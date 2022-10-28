6dde2c90037c9dc9c1955819c17ed2424b78e144
«Vadim <Hazz74@yandex.ru>
2022-10-28 23:48:00 +0500

    write cod

--- Program.cs
+++ Program.cs
@@ -46,6 +46,39 @@
 // 46 -> нет 
 // 161 -> да
 
-Console.WriteLine("Введите натуральное число ");
-int x=Convert.ToInt32 ( Console.ReadLine());
+// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 
+// 7 и 23.
+
+// Console.WriteLine("Введите натуральное число ");
+// int x=Convert.ToInt32 ( Console.ReadLine());
+// if (x % 7 == 0 && x % 23 ==0) Console.Write("Да");
+// else Console.Write("Нет");
+
+// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+// 5, 25  ->  да
+// -4, 16  ->  да
+// 25, 5  ->  да
+// 8,9  ->  нет
+
+// Console.WriteLine("Введите натуральное число ");
+// int x=Convert.ToInt32 ( Console.ReadLine());
+// Console.WriteLine("Введите натуральное число ");
+// int y=Convert.ToInt32 ( Console.ReadLine());
+// if (x*x == y || y*y == x) Console.Write("Да");
+// else Console.Write("Нет");
+
+// Console.WriteLine("Введите натуральное число ");
+// int x=Convert.ToInt32 ( Console.ReadLine());
+// Console.WriteLine("Введите натуральное число ");
+// int y=Convert.ToInt32 ( Console.ReadLine());
+// if (x*x == y ) Console.Write("Да");
+// else if ( y*y == x) Console.Write("Да");
+// else Console.Write("Нет");
+
+// Напишите программу, которая выводит целое  число любой разрядности и удаляет вторую цифру слева этого числа.
+
+
+
+
 
