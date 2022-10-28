15dac4033a436fa78e6e80a638090350bb84c1aa
«Vadim <Hazz74@yandex.ru>
2022-10-28 23:05:14 +0500

    rf

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому.
+// Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+
+Console.WriteLine("Введите натуральное число ");
+int x=Convert.ToInt32 ( Console.ReadLine());
+Console.WriteLine("Введите натуральное число ");
+int y=Convert.ToInt32 ( Console.ReadLine());
+int i= x % y;
+if (i == 0) Console.WriteLine("Кратно");
+ else  Console.WriteLine("не кратно, остаток " + i);
